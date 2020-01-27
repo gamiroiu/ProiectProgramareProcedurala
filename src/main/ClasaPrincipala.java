e9a46f933a350fc5a14db8735ac160aa6c50a75b
Gabriela Miroiu <gabriela_miroiu@yahoo.com>
2020-01-27 20:27:05 +0200

    Al treilea commit

--- src/main/ClasaPrincipala.java
+++ src/main/ClasaPrincipala.java
@@ -2,9 +2,13 @@ package main;
 
 public class ClasaPrincipala {
 
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		System.out.println("Hello World!");
 
 	}
-
+    public static void main(String[] args) {
+    	Mediu m = new Mediu(); //aici se apeleaza clasa Mediu
+    	m.method1(); //apeleaza metoda method1 din clasa Mediu
+    	m.method2(); //apeleaza metoda method2 din clasa Mediu
+    }
 }

032fe04d9844456b3172db48260167ca501ff757
Gabriela Miroiu <gabriela_miroiu@yahoo.com>
2020-01-29 21:19:57 +0200

    Corectare exercitii

--- src/main/Asignare.java
+++ src/main/Asignare.java
@@ -7,13 +7,14 @@ public class Asignare {
 		    	   int b = (a=2)*a;
 		    	   int c = b * (b=5);
 		    	   System.out.println("a=" + a + ",b=" + b + ",c=" + c);
+/*sunt afisate urmatoarele rezultate: a=2;b=5 si c=20
+*a=2 deoarece este reinitializat la expresia de la linia 7 (a=2)
+*b= 2*2=4 iar acesta este reinitializat la expresia de la linia 8 (b=5)
+*c=20 deoarece 4*5=20
+*/
 		    	   double d = 2.95;
 		    	   int i = 4;
 		    	   System.out.println(++d>i?d:i);
-		    	   int suma = 0;
-		    	   for(i = 1, i < 10; i++ ) {
-		    		   suma += suma + i;
-		    	   }
-		    	   System.out.print("Suma este: " + suma);
+//se compileaza si afiseaza valoarea 4.0
 		       }
 }
--- src/main/Ex3.java
+++ src/main/Ex3.java
@@ -5,9 +5,11 @@ public class Ex3 {
 	public static void main(String[] args) {
 		int a = 3;
 		if (++a < 4)
+			if (a++ < 4)
 			System.out.println(a);
 		else
 			System.out.println(a);
 	}
+//nu este afisat niciun rezultat
 
 }

b75a18db2978e445e074a91929975208e394c797
Gabriela Miroiu <gabriela_miroiu@yahoo.com>
2020-01-30 21:04:03 +0200

    Modificari

--- src/main/Asignare.java
+++ src/main/Asignare.java
@@ -1,7 +1,7 @@
 package main;
 
 public class Asignare {
-
+//TEMA1
 	public static void main(String[] args) {
 		    	   int a= 3;
 		    	   int b = (a=2)*a;
@@ -12,9 +12,18 @@ public class Asignare {
 *b= 2*2=4 iar acesta este reinitializat la expresia de la linia 8 (b=5)
 *c=20 deoarece 4*5=20
 */
+//TEMA2
 		    	   double d = 2.95;
 		    	   int i = 4;
 		    	   System.out.println(++d>i?d:i);
 //se compileaza si afiseaza valoarea 4.0
-		       }
+		    	   
+//TEMA 4
+		    		int suma = 0;
+		    		for(int i1 = 1; i1 <10; i1++){
+		    		        suma += suma + i1;
+		    		}
+		    		System.out.println("Suma este: " +suma);
+//Aceasta functie se complieaza iar suma este: 1013
+		    	}
 }
---
+++ src/main/TemaEx3.java
@@ -0,0 +1,15 @@
+package main;
+
+public class TemaEx3 {
+//TEMA 3
+	public static void main(String[] args) {
+		int a = 3;
+		if(++a <4)
+		if(++a <4)
+			System.out.println(a);
+		else
+			System.out.println(a);
+// Aceasta functie se compileaza dar nu afiseaza nimic intrucat a=3; 
+//prima expresie va fi citita ca 4<4 si nu se incadreaza in prima conditie, asadar nu mai trece la urmatoarea conditie
+	}
+}
